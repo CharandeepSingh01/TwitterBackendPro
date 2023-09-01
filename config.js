cef61a0630f0d04a7682aa3e9923c12c30f8dd10
CharandeepSingh01 <login.charandeep@gmail.com>
2023-09-01 23:53:41 +0530

    production

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGOBD_URL: "mongodb://127.0.0.1:27017/test",
+    MONGOBD_URL: "mongodb+srv://12011066:12011066@cluster0.pwegexw.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
 }
